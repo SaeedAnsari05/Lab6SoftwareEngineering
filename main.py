08ec6d27db1100255733ca4f1eac11d087945fd2
SaeedAnsari05 <saeeduhansari@gmail.com>
2023-12-01 18:31:46 -0500

    board functions

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+name = input("Hello. what is your name?")
+age = input("It's nice to meet you, " + name + ". How old are you?")
+print("I see that you are still quite young at only " + age + ".")
+location = input("Where do you live?")
+print("Wow! I've always wanted to go to " + location + ". Thanks for chatting with me. Bye!")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
